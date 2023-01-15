3269d81ba72374df95f942039f2aab3390e61d0e
JinSung Hwang <jinsunghwang@HJS.local>
2023-01-15 15:54:55 +0900

    feature: 유저 서비스 및 모듈 분리
    5장 유저 서비스의 모듈 분리

---
+++ src/users/users.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Query,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email-dto';
+import { UserLoginDto } from './dto/user-login-dto';
+import { UserInfo } from './dto/user-info.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() dto: CreateUserDto): Promise<void> {
+    const { name, email, password } = dto;
+    await this.usersService.createUser(name, email, password);
+  }
+
+  @Get('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto) {
+    const { signupVerifyToken } = dto;
+
+    return await this.usersService.verifyEmail(signupVerifyToken);
+  }
+
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    const { email, password } = dto;
+
+    return await this.usersService.login(email, password);
+  }
+
+  @Get('/:id')
+  async getUserInfo(@Param('id') userId: string): Promise<UserInfo> {
+    return await this.usersService.getUserInfo(userId);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string) {
+    return this.usersService.remove(+id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,54 @@
+import * as uuid from 'uuid';
+import { Injectable } from '@nestjs/common';
+import { EmailService } from '../email/email.service';
+import { UserInfo } from './dto/user-info.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private emailService: EmailService) {}
+
+  async createUser(name: string, email: string, password: string) {
+    await this.checkUserExist(email);
+
+    const signupVerifyToken = uuid.v1();
+
+    await this.saveUser(name, email, password, signupVerifyToken);
+    await this.sendMemberJoinEmail(email, signupVerifyToken);
+  }
+
+  private checkUserExist(email: string) {
+    return false;
+  }
+
+  private saveUser(
+    name: string,
+    email: string,
+    password: string,
+    signupVerifyToken: string,
+  ) {
+    return;
+  }
+
+  private async sendMemberJoinEmail(email: string, signupVerifyToken: string) {
+    await this.emailService.sendMemberJoinVerification(
+      email,
+      signupVerifyToken,
+    );
+  }
+
+  async verifyEmail(signupVerifyToken: string) {
+    return;
+  }
+
+  async getUserInfo(userId: string): Promise<UserInfo> {
+    throw new Error('Method not implemented.');
+  }
+
+  async login(email: string, password: string): Promise<string> {
+    throw new Error('Method not implemented');
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
