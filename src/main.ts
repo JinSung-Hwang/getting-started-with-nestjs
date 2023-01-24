83408355a468ff67f4da29d5501dcfac6f7108b4
JinSung Hwang <jinsunghwang@HJS.local>
2023-01-24 22:59:12 +0900

    feature: 요청 처리 전에 부가 기능을 수행하기 위한 미들웨어 추가

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { logger } from './common/logger/logger.middleware';
 
-async function bootstrap() {
+(async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(logger);
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
     }),
   );
   await app.listen(3000);
-}
-bootstrap();
+})();
